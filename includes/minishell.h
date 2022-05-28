2b617327db86ec145c1b0fa8895a6359c03b52fb
kny-i <kny_i@keio.jp>
2022-05-28 16:21:29 +0900

    add include

---
+++ includes/minishell.h
@@ -0,0 +1,13 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <libc.h>
+# include "../libft/libft.h"
+
+
+#endif // !MINISHELL_H
