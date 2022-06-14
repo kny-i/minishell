017fea24b7326be883a63a876a906293c0ecbe6d
kny-i <kny_i@keio.jp>
2022-06-15 03:42:24 +0900

    add get_input

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -17,10 +17,26 @@ void execute(t_cmd **cmd, t_envp *envp)
 {
 	t_cmd *tmp_cmd;
 	int cmd_num;
+	int i;
+	int fd[2][2];
 	int *pid;
 
 	tmp_cmd = *cmd;
 	cmd_num = count_cmd(tmp_cmd);
-	pid = (int *)x_calloc()
+	pid = (int *)x_calloc(cmd_num, sizeof(int));
+	i = 0;
+	while (i < cmd_num)
+	{
+		if (i != cmd_num - 1)
+			x_pipe(fd[i % 2]);
+		pid[i] = x_fork();
+		if (pid[i] == 0)
+		{
+			get_input(tmp_cmd, i, fd);
+			get_output(tmp_cmd, i, fd);
+
+		}
+		i++;
+	}
 
 }
