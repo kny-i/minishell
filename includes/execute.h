3b434fb0d1c8c4a41a4f879fc7b4b9e4c7d85807
kny-i <kny_i@keio.jp>
2022-06-15 02:49:33 +0900

    add cmd count func

--- includes/execute.h
+++ includes/execute.h
@@ -6,6 +6,6 @@
 # include "./env.h"
 
 typedef struct s_lexer	t_lexer;
-void execute(t_cmd **cmd, t_env *env);
-
+void execute(t_cmd **cmd, t_envp *env);
+int count_cmd(t_cmd *cmd);
 #endif
--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -1,5 +1,17 @@
 #include "execute.h"
 
+int count_cmd(t_cmd *cmd)
+{
+	int cmd_cnt;
+
+	cmd_cnt = 0;
+	while (cmd != NULL)
+	{
+		cmd_cnt++;
+		cmd = cmd->next;
+	}
+	return (cmd_cnt);
+}
 void execute(t_cmd **cmd, t_envp *envp)
 {
 	t_cmd *tmp_cmd;
