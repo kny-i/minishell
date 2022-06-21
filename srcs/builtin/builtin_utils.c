48698c32ca207e3701c321c12c16283a103b6ce0
yukikondo <kny_>
2022-06-22 03:41:25 +0900

    modify is_only_builtin

--- srcs/builtin/builtin_utils.c
+++ srcs/builtin/builtin_utils.c
@@ -1,19 +1,18 @@
 #include "builtin.h"
 #include "utils.h"
 
-bool	is_only_builtin(t_lexer *lexerbuf)
+bool	is_only_builtin(t_cmd *cmd)
 {
-	char *cmd;
-	if (lexerbuf->num_token != 1)
-		return (0);
-	cmd = lexerbuf->list_token->data;
-	return (ft_strcmp("cd", cmd) == 0
-			|| ft_strcmp("echo", cmd) == 0
-			|| ft_strcmp("env", cmd) == 0
-			|| ft_strcmp("exit", cmd) == 0
-			|| ft_strcmp("export", cmd) == 0
-			|| ft_strcmp("pwd", cmd) == 0
-			|| ft_strcmp("unset", cmd) == 0);
+	char *cmd_name;
+
+	cmd_name = cmd->cmd;
+	return (ft_strcmp("cd", cmd_name) == 0
+			|| ft_strcmp("echo", cmd_name) == 0
+			|| ft_strcmp("env", cmd_name) == 0
+			|| ft_strcmp("exit", cmd_name) == 0
+			|| ft_strcmp("export", cmd_name) == 0
+			|| ft_strcmp("pwd", cmd_name) == 0
+			|| ft_strcmp("unset", cmd_name) == 0);
 }
 
 bool	is_builtin(char *cmd)
