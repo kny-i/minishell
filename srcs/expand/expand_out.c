454a05d12fd8bde10ed8af17096f60e742e60470
rm-ryou <emco.420118@gmail.com>
2022-07-06 08:22:15 +0900

    fix expand

--- srcs/expand/expand_out.c
+++ srcs/expand/expand_out.c
@@ -1,43 +1,43 @@
 #include "expand.h"
 #include "utils.h"
 
-void	ft_free_node(t_list *list)
+void	ft_free_node(t_list **list)
 {
 	t_list	*free_node;
 
-	free_node = list;
-	list = list->next;
+	free_node = *list;
+	(*list) = (*list)->next;
 	ft_lstdelone(free_node, free);
 }
 
-void	update_cmd(t_cmd *cmd)
+void	update_cmd_out(t_cmd *cmd)
 {
-	t_list	*free_node;
-
-	if (cmd->args->content[0] == '>')
-	{
-		ft_free_node(cmd->args);
-	}
-	ft_free_node(cmd->args);
-	cmd->cmd = for_free(ft_strdup(cmd->args->content), cmd->cmd);
-	ft_free_node(cmd->args);
-}
-
-static int	check_cmd(t_cmd *cmd)
-{
-	t_list  *tmp;
+	t_list	*tmp;
 
 	tmp = cmd->args;
+	while (tmp != NULL)
+	{
+		if (cmd->args->content[0] == '>')
+			ft_free_node(&tmp);
+		else
+			tmp = tmp->next;
+	}
+}
+
+static int	check_cmd(t_cmd *cmd, t_list *args)
+{
 	if (cmd->cmd[0] == '>')
 	{
-		if (tmp->content[0] == '>')
-			cmd->fd_out = open(tmp->next->content, O_WRONLY | O_CREAT | O_APPEND, S_IWUSR | S_IRUSR);
+		if (args->content[0] == '>')
+			cmd->fd_out = open(args->next->content, O_WRONLY | O_CREAT | O_APPEND, S_IWUSR | S_IRUSR);
 		else
-			cmd->fd_out = open(tmp->content, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
+			cmd->fd_out = open(args->content, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
 		if (cmd->fd_out == -1)
+		{
+			printf("minishell: %s: Is a directory\n", args->content);
 			return (1);
+		}
 	}
-//	update_cmd(cmd);
 	return (0);
 }
 
@@ -46,7 +46,7 @@ int	check_redirect_out(t_cmd *cmd_list)
 	t_list	*tmp;
 
 	tmp = cmd_list->args;
-	if (check_cmd(cmd_list))
+	if (check_cmd(cmd_list, tmp))
 		return (1);
 	while (tmp != NULL)
 	{
@@ -65,5 +65,6 @@ int	check_redirect_out(t_cmd *cmd_list)
 		}
 		tmp = tmp->next;
 	}
+//	update_cmd_out(cmd_list);
 	return (0);
 }
