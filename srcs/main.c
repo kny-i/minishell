6ae318a8a10d32ce1971d8201eab8a2b378bbb8e
yukikondo <kny_>
2022-07-06 13:33:42 +0900

    add main

---
+++ srcs/main.c
@@ -0,0 +1,83 @@
+#include "minishell.h"
+#include "lexer.h"
+#include "parser.h"
+#include "env.h"
+#include "expand.h"
+#include "free.h"
+
+void    print_env(t_envp *env)
+{
+	t_envp    *tmp;
+
+	tmp = env;
+	for (; tmp != NULL; tmp = tmp->next)
+	{
+		printf("name     = %s\n", tmp->env_name);
+		printf("contents = %s\n", tmp->content);
+	}
+}
+
+void    print_cmd_list(t_cmd *cmd_list)
+{
+	t_cmd    *tmp;
+
+	tmp  = cmd_list;
+	for(; tmp != NULL; tmp = tmp->next)
+	{
+		printf("cmd = %s\n", tmp->cmd);
+		for (t_list *list = tmp->args; list != NULL; list = list->next)
+			printf("%s ", list->content);
+		putchar('\n');
+	}
+}
+
+void    free_env_list(t_envp *env_list)
+{
+	t_envp    *tmp;
+
+	while (env_list != NULL)
+	{
+		tmp = env_list->next;
+		free(env_list->content);
+		free(env_list->env_name);
+		free(env_list);
+		env_list = tmp;
+	}
+}
+
+void    minishell(char *environ[])
+{
+	char    *line;
+	t_cmd *cmd_list;
+	t_envp *env_list;
+
+	env_list = creat_tenv(environ);
+	sig_input();
+	while (1)
+	{
+		line = readline("minishell> ");
+		if (line == NULL)
+			break;
+		add_history(line);
+		cmd_list = lex_pars(line);
+		if (expand(&cmd_list, &env_list))
+		{
+			print_cmd_list(cmd_list);
+			//    execute(&cmd_list, &env_list);
+		}
+		free_cmd(cmd_list);
+		//    free_env_list(env_list);
+		//    system("leaks minishell");
+	}
+	printf("exit minishell\n");
+	exit(0);
+}
+
+int    main(int argc, char *argv[])
+{
+	extern char    **environ;
+
+	if (argc == 1)
+		minishell(environ);
+	return (0);
+}
