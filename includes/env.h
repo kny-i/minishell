50d096fa7746851d5a93460c33c4a53996101586
kny-i <kny_i@keio.jp>
2022-07-07 16:55:37 +0900

    norm envh

--- includes/env.h
+++ includes/env.h
@@ -1,22 +1,21 @@
 #ifndef ENV_H
-#define ENV_H
+# define	ENV_H
 
 # include "./minishell.h"
 
-
 typedef struct s_envp
 {
-	struct s_envp *next;
-	char		*env_name;
-	char 		*content;
+	struct s_envp	*next;
+	char			*env_name;
+	char			*content;
 }				t_envp;
 
-void set_status(char **envp, int status);
-char *get_env_content(t_envp *env, char *name);
-t_envp *create_tenv(char **envp);
-t_envp *ft_envnew(char *env_name, char *content);
+void	set_status(char **envp, int status);
+char	*get_env_content(t_envp *env, char *name);
+t_envp	*create_tenv(char **envp);
+t_envp	*ft_envnew(char *env_name, char *content);
 char	*get_path(t_envp *envp);
-int	cnt_env(t_envp *envp);
+int		cnt_env(t_envp *envp);
 void	create_tenv_utils(char **envp, t_envp *env_tmp);
 
 #endif
