381ef1f34825970f037330f5e2a1e9577217ef1f
kny-i <kny_i@keio.jp>
2022-07-07 16:57:03 +0900

    norm builtin h

--- includes/builtin.h
+++ includes/builtin.h
@@ -1,22 +1,21 @@
 #ifndef BUILTIN_H
-#define BUILTIN_H
+# define BUILTIN_H
 
 # include "./minishell.h"
 # include "./parser.h"
 typedef struct s_cmd	t_cmd;
 typedef struct s_lexer	t_lexer;
-typedef struct s_envp    t_envp;
-
+typedef struct s_envp	t_envp;
 bool	is_builtin(t_cmd *cmd);
-int	execute_builtin(t_cmd *cmd_list,t_envp **enpvp, char **darray_args);
-int cd_core(char **args, t_envp *env);
-int	export_core(char **args, t_envp **env);
-int echo_core(t_cmd *cmd_line, char **str);
-int pwd_core(void);
+int		execute_builtin(t_cmd *cmd_list, t_envp **enpvp, char **darray_args);
+int		cd_core(char **args, t_envp *env);
+int		export_core(char **args, t_envp **env);
+int		echo_core(t_cmd *cmd_line, char **str);
+int		pwd_core(void);
 //int env_core(t_cmd *cmd, t_envp *env)
-int env_core(t_envp *env);
-int	unset_core(char **args, t_envp **envp);
-int exit_core(char **args);
+int		env_core(t_envp *env);
+int		unset_core(char **args, t_envp **envp);
+int		exit_core(char **args);
 void	env_add_back(t_envp **envp, t_envp *new);
 bool	has_char(const char *str, char c);
 char	*strcpy_untill_c(char *dst, const char *src, char c);
