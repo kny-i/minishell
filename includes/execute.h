f78f02bf299abea8ed7e921ebde99c7f46eb5173
kny-i <kny_i@keio.jp>
2022-07-07 16:58:00 +0900

    norm executeh

--- includes/execute.h
+++ includes/execute.h
@@ -1,20 +1,20 @@
 #ifndef EXECUTE_H
-#define EXECUTE_H
+# define EXECUTE_H
 
 # include "./minishell.h"
 # include "./lexer.h"
 # include "./env.h"
 # include "./parser.h"
 typedef struct s_cmd	t_cmd;
-typedef struct s_envp    t_envp;
+typedef struct s_envp	t_envp;
 
 typedef struct s_lexer	t_lexer;
-int count_cmd(t_cmd *cmd);
+int		count_cmd(t_cmd *cmd);
 char	**list_to_args(t_cmd *cmd);
-int execute_test(t_cmd **cmd_list, t_envp **envp);
+int		execute_test(t_cmd **cmd_list, t_envp **envp);
 char	**list_to_env(t_envp *envp);
-int	count_cmd(t_cmd *cmd);
-int	get_list_size(t_list *args);
+int		count_cmd(t_cmd *cmd);
+int		get_list_size(t_list *args);
 char	**list_to_args(t_cmd *cmd);
 void	list_to_args_loop(t_list *tmp_args, int *len, char **res, t_cmd	*cmd);
 void	execute_abs(char **args, char *cmd);
@@ -22,6 +22,6 @@ void	execve_not_builtin(char **path_tmp, t_cmd *cmd_list, \
 										char **args, int *res);
 void	close_dup(int fd, int oldfd, int newfd, bool flg);
 void	close_parents_fd(int i, int **fd);
-int	execve_cmd(t_cmd *cmd_list, char **env_path_split, t_envp **envp);
+int		execve_cmd(t_cmd *cmd_list, char **env_path_split, t_envp **envp);
 
 #endif
