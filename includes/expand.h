2f1e1e8a566aa827947df13c6f66af168316dfd2
kny-i <kny_i@keio.jp>
2022-07-20 15:46:56 +0900

    norm expand

--- includes/expand.h
+++ includes/expand.h
@@ -4,5 +4,9 @@
 # include "./minishell.h"
 
 bool	expand(t_cmd *cmd, t_envp *envp);
-
+bool	open_redirect(t_cmd *cmd_list);
+bool	launch_expand_redirect(t_cmd *cmd_list);
+int		redirect_open_out(char *filename, bool is_append, int *flg);
+int		redirect_open_in(char *filename, int *flg);
+int		launch_heredoc(char *end_str, int *flg);
 #endif
